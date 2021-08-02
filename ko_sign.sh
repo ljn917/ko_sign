0adf949da50a48b143e55a60365eb115ff50860d
Jiannan Liu <ljn917@gmail.com>
2021-08-02 14:17:23 -0400

    ko_sign.sh: Add interactive mode

--- ko_sign.sh
+++ ko_sign.sh
@@ -2,6 +2,7 @@
 
 source ./env.sh
 
+KERNEL_BASE_DIR="/lib/modules"
 KERNEL_VERSION=$(uname -r)
 if [ $# -ge 1 ]; then
     KERNEL_VERSION=$1
@@ -13,7 +14,24 @@ function sign() {
     /usr/src/kernels/${KERNEL_VERSION}/scripts/sign-file sha256 "${KEY}" "${CERT}" "$1"
 }
 
-cd /lib/modules/${KERNEL_VERSION}/extra/nvidia/
-for module_path in $(ls /lib/modules/${KERNEL_VERSION}/extra/nvidia/nvidia*); do
+if [[ $1 == "-i" ]]; then
+    ALL_KERNEL_VERSIONS=(`ls ${KERNEL_BASE_DIR}`)
+    for i in "${!ALL_KERNEL_VERSIONS[@]}"; do
+        echo "${i}: ${ALL_KERNEL_VERSIONS[i]}"
+    done
+    read -p "Enter option (default ${ALL_KERNEL_VERSIONS[-1]}): " option
+    if [ -z "$option" ]; then
+        option="-1"
+    fi
+    KERNEL_VERSION=${ALL_KERNEL_VERSIONS[option]}
+    echo "Selected kernel version: ${KERNEL_VERSION}"
+    if [ -z "$KERNEL_VERSION" ]; then
+        echo "ERROR: Option (${option}) is not valid."
+        exit -1
+    fi
+fi
+
+cd ${KERNEL_BASE_DIR}/${KERNEL_VERSION}/extra/nvidia/
+for module_path in $(ls ${KERNEL_BASE_DIR}/${KERNEL_VERSION}/extra/nvidia/nvidia*); do
     sign "${module_path}"
 done
